2011620d85b3415cf5977f4cbed7ba4c343fc626
MikeColeone <coleonemike@gmail.com>
2024-09-23 23:00:28 +0800

    更新数据表

---
+++ src/main/java/org/example/graduatemanage/GraduateManageApplication.java
@@ -0,0 +1,13 @@
+package org.example.graduatemanage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GraduateManageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GraduateManageApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/example/graduatemanage/GraduateManageApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.graduatemanage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GraduateManageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
